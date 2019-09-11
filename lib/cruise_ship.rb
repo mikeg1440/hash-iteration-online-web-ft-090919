401fafcf9d416eed93c3f59768e6c04525afb95f
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-11 00:30:38 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,24 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
-  # add the code snippet here!
+
+  winner = ""
+  
+  passengers.each do |suite, name|
+  
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+
+  end
+  
+  # winner = passengers[:suite_a][0] == "A" ? passengers[:suite_a] : nil
+  
+  winner
 end
